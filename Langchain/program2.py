8cea22a9d23a8631d307c9a0be8dae3492ac08b0
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2024-05-13 13:59:15 +0530

    langchain program

---
+++ Langchain/program2.py
@@ -0,0 +1,7 @@
+API_KEY ="..."
+
+from langchain import HuggingFaceHub
+
+llm = HuggingFaceHub(repo_id = "google/flan-t5-xl", huggingfacehub_api_token = API_KEY)
+
+print(llm("Tell me a joke about data scientist"))
