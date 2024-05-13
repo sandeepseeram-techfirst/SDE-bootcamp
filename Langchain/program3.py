82edb74ea3b0c5b0a9f68f17a202df1e51848876
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2024-05-13 13:57:22 +0530

    langchain program

---
+++ Langchain/program3.py
@@ -0,0 +1,21 @@
+USER_INPUT = 'Paris'
+
+from langchain.llms import OpenAI
+
+from langchain import PromptTemplate
+
+llm = OpenAI(model_name="text-davinci-003", openai_api_key=API_KEY)
+
+template = """ I am travelling to {location}. What are the top 3 things I can do while I am there. Be very specific and respond as three bullet points """
+
+prompt = PromptTemplate(
+
+input_variables=["location"],
+
+template=template,
+
+)
+
+final_prompt = prompt.format(location=USER_INPUT )
+
+print(f"LLM Output: {llm(final_prompt)}")
