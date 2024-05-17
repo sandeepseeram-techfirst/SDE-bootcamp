8b9903ce456a88ea113e94e9777a589562f3d942
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2024-05-17 14:45:24 +0530

    nodejs app

--- nodejs/db.js
+++ nodejs/db.js
@@ -1,7 +1,7 @@
 const db = [
     { id: 1, name: 'John' },
-    { id: 2, name: 'Jane' },
-    { id: 3, name: 'Joe' },
+    { id: 2, name: 'Jane' }, 
+    { id: 3, name: 'Joe' }, 
     { id: 4, name: 'Jack' },
     { id: 5, name: 'Jill' },
     { id: 6, name: 'Jak' },
