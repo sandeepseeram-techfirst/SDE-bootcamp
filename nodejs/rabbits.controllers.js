3e9fae1385436563511902930dc249ee8722ce65
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2024-05-17 14:44:47 +0530

    nodejs app

---
+++ nodejs/rabbits.controllers.js
@@ -0,0 +1,51 @@
+const { getAllItems, getItem, editItem, addItem, deleteItem } = require('../models/rabbits.models')
+
+const listRabbits = (req, res) => {
+    try {
+        const resp = getAllItems()
+        res.status(200).send(resp)
+
+    } catch (err) {
+        res.status(500).send(err)
+    }
+}
+
+const getRabbit = (req, res) => {
+    try {
+        const resp = getItem(parseInt(req.params.id))
+        res.status(200).send(resp)
+
+    } catch (err) {
+        res.status(500).send(err)
+    }
+}
+
+const editRabbit = (req, res) => {
+    try {
+        const resp = editItem(req.params.id, req.body.item)
+        res.status(200).send(resp)
+    } catch (err) {
+        res.status(500).send(err)
+    }
+}
+
+const addRabbit = (req, res) => {
+    try {
+        console.log( req.body.item )
+        const resp = addItem(req.body.item)
+        res.status(200).send(resp)
+    } catch (err) {
+        res.status(500).send(err)
+    }
+}
+
+const deleteRabbit = (req, res) => {
+    try {
+        const resp = deleteItem(req.params.idx)
+        res.status(200).send(resp)
+    } catch (err) {
+        res.status(500).send(err)
+    }
+}
+
+module.exports = { listRabbits, getRabbit, editRabbit, addRabbit, deleteRabbit }
