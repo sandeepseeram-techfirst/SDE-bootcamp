cd1b19360b81d8bb88200ea6995e11f7f8c6d448
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2024-05-23 17:05:40 +0530

    springboot MockMVC

--- SpringBoot/controller.java
+++ SpringBoot/controller.java
@@ -1,4 +1,4 @@
-package com.sandeepseeram.mockmvcbooks;
+package com.bookapplication.mockmvcbooks;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
