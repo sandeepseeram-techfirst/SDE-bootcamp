79a8932f8d977a35bca8ec771fc2f96f95f62fef
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2024-05-24 15:52:18 +0530

    terraform

--- Platform_Engineering/Terraform/aws_infra/main.tf
+++ Platform_Engineering/Terraform/aws_infra/main.tf
@@ -5,7 +5,7 @@ resource "aws_s3_bucket" "example" {
 #resource "aws_s3_bucket_acl" "example" {
 #  bucket = aws_s3_bucket.example.id
 #  acl    = "private"
-#}
+#} 
 
 resource "aws_s3_bucket_versioning" "versioning_example" {
   bucket = aws_s3_bucket.example.id
