1dc6f3b10cc58e0bea7ed0a014264a9d333e6a36
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2024-05-25 17:16:16 +0530

    springboot MockMVC

---
+++ SpringBoot/bookscontroller.mockmvc.java
@@ -0,0 +1,13 @@
+package com.booksapplication.mockmvcbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockMvcBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockMvcBooksApplication.class, args);
+    }
+
+}
