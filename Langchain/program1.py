6a4e09501d86552dbed33d03c10d1771619fe852
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2024-06-08 14:38:17 +0530

    platform engineering bootcamp

--- Langchain/program1.py
+++ Langchain/program1.py
@@ -4,4 +4,4 @@ from langchain.llms import OpenAI
 
 llm = OpenAI(model_name="text-ada-001", openai_api_key=API_KEY)
 
-print(llm("Tell me a joke about data scientist"))
+print(llm("Tell me a joke about data scientist")) 
