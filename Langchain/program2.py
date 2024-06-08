adcf09f5e581b94cb6bfc349714a9c5d82bb32df
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2024-06-08 14:38:23 +0530

    platform engineering bootcamp

--- Langchain/program2.py
+++ Langchain/program2.py
@@ -1,6 +1,6 @@
 API_KEY ="..."
 
-from langchain import HuggingFaceHub
+from langchain import HuggingFaceHub 
 
 llm = HuggingFaceHub(repo_id = "google/flan-t5-xl", huggingfacehub_api_token = API_KEY)
 
