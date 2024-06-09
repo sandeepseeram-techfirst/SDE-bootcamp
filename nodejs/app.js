6659ba7fc168a4abfd450981edbd74a8a6f1f2b4
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2024-06-09 14:38:59 +0530

    platform engineering bootcamp

--- nodejs/app.js
+++ nodejs/app.js
@@ -2,7 +2,7 @@ const express = require('express');
  
 const rabbitRoutes = require('./rabbits/routes/rabbits.routes')
 
-const app = express()
+const app = express() 
 const port = 7070
 
 /* Routes */ 
