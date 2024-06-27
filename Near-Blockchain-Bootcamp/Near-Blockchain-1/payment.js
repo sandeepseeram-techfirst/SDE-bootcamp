8bc0a3641b76f6bef411f08a9295fc51a64da840
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2024-06-27 16:31:22 +0530

    near-blockchain-workshop

--- Near-Blockchain-Bootcamp/Near-Blockchain-1/payment.js
+++ Near-Blockchain-Bootcamp/Near-Blockchain-1/payment.js
@@ -1,5 +1,3 @@
-// Integrate the payment gateway in line 20 to start accepting payments in NEAR.
-// Make sure you do not add extra spaces for code validation purposes.
 const nearAPI = require("near-api-js");
 const { connect, utils } = nearAPI;
 const { config } = require('./config'); // loads config settings
