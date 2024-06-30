1bf63daaecfca5005b27befd25d51d79997ee36b
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2024-06-30 16:51:13 +0530

    near-blockchain-workshop

--- Near-Blockchain-Bootcamp/Near-Blockchain-1/app/frontend/wallet.js
+++ Near-Blockchain-Bootcamp/Near-Blockchain-1/app/frontend/wallet.js
@@ -12,3 +12,19 @@ import { setupMyNearWallet } from '@near-wallet-selector/my-near-wallet';
 const THIRTY_TGAS = '30000000000000';
 const NO_DEPOSIT = '0';
 
+export class Wallet {
+    /**
+     * @constructor
+     * @param {Object} options - the options for the wallet
+     * @param {string} options.networkId - the network id to connect to
+     * @param {string} options.createAccessKeyFor - the contract to create an access key for
+     * @example
+     * const wallet = new Wallet({ networkId: 'testnet', createAccessKeyFor: 'contractId' });
+     * wallet.startUp((signedAccountId) => console.log(signedAccountId));
+     */
+    constructor({ networkId = 'testnet', createAccessKeyFor = undefined }) {
+      this.createAccessKeyFor = createAccessKeyFor;
+      this.networkId = networkId;
+    }
+  
+    
