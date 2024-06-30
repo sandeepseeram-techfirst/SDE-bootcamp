5be163c7d0fd5157f3b54b5ba45b94e49b888ca0
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2024-06-30 16:50:50 +0530

    near-blockchain-workshop

---
+++ Near-Blockchain-Bootcamp/Near-Blockchain-1/app/frontend/wallet.js
@@ -0,0 +1,14 @@
+// near api js
+import { providers } from 'near-api-js';
+
+// wallet selector
+import { distinctUntilChanged, map } from 'rxjs';
+import '@near-wallet-selector/modal-ui/styles.css';
+import { setupModal } from '@near-wallet-selector/modal-ui';
+import { setupWalletSelector } from '@near-wallet-selector/core';
+import { setupHereWallet } from '@near-wallet-selector/here-wallet';
+import { setupMyNearWallet } from '@near-wallet-selector/my-near-wallet';
+
+const THIRTY_TGAS = '30000000000000';
+const NO_DEPOSIT = '0';
+
