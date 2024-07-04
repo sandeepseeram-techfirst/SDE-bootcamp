b476428a83e2df916b4ed3ff24c5d573c7916c96
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2024-07-04 17:13:05 +0530

    near-blockchain-workshop

--- Near-Blockchain-Bootcamp/Near-Blockchain-1/payment.js
+++ Near-Blockchain-Bootcamp/Near-Blockchain-1/payment.js
@@ -3,7 +3,7 @@ const { connect, utils } = nearAPI;
 const { config } = require('./config'); // loads config settings
 
 // configure accounts, network, and amount of NEAR to send
-const sender = "sender.testnet";
+const sender = "sender.testnet"; 
 const receiver = "receiver.testnet";
 const networkId = "testnet";
 const amount = utils.format.parseNearAmount("1.5");
